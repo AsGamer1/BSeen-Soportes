002cbd371846578fca614f104dbde91351f16bfe
AsGamer1 <asilva.ys@gmail.com>
2024-07-14 13:59:48 +0200

    head and css

---
+++ app/head.js
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <head>
+      <meta name="title" content="B Seen Media - Soportes" />
+      <meta name="description" content="Aplicación de consulta de los soportes convencionales y digitales de B Seen Media" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <link rel="manifest" href="/api/manifest" />
+      <meta name="theme-color" content="#039dde" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    </head>
+  )
+}
