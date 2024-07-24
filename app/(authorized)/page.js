1443797a74ccf141ce94b4417f022051f86a2c94
AsGamer1 <asilva.ys@gmail.com>
2024-07-24 12:55:26 +0200

    minor layout edits

--- app/(authorized)/page.js
+++ app/(authorized)/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Drawer, Stack, Typography } from "@mui/material";
+import { Box, Drawer, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('@/app/_components/map'), { ssr: false })
@@ -23,7 +23,13 @@ export default function Dashboard() {
       <Map setSelectedLugar={setSelectedLugar} setIsOpen={setIsOpen} setSelectedSoportes={setSelectedSoportes} setColor={setColor} />
       <Drawer anchor="right" open={isOpen} onClose={DrawerClose}>
         <Stack sx={{ width: "calc(min(320px, 70vw))", overflow: "hidden" }}>
-          <Typography variant="h6" sx={{ padding: 3, color: "white", bgcolor: color, minHeight: "2rem", textAlign: "center" }}>{selectedLugar}</Typography>
+          <Box sx={{ padding: 3, color: "white", bgcolor: color, minHeight: "2rem", textAlign: "center" }}>
+            {selectedLugar.split("/n").map(lugar =>
+              <>
+                <Typography variant="h6">{lugar}</Typography>
+              </>
+            )}
+          </Box>
           {selectedSoportes?.map(soporte =>
             <>
               <Typography>{soporte.nombre}</Typography>
